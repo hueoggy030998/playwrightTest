34d0965132f4a7ef0953dbadd99a2c99b57f4f0b
hueoggy030998 <huetransky@gmail.com>
2024-07-31 16:09:50 +0700

    test

---
+++ src/main/java/Test.java
@@ -0,0 +1,31 @@
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class Test {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+
+            // Start tracing before creating / navigating a page.
+            context.tracing().start(new Tracing.StartOptions()
+                    .setScreenshots(true)
+                    .setSnapshots(true)
+                    .setSources(true));
+
+            Page page = context.newPage();
+            page.navigate("https://test-aitriage-bo.beedu.vn/");
+
+            // Expect a title "to contain" a substring.
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+
+            // Stop tracing and export it into a zip archive.
+            context.tracing().stop(new Tracing.StopOptions()
+                    .setPath(Paths.get("trace.zip")));
+        }
+    }
+}
